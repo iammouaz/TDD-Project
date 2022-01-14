649a4433a50ab0611f5b256786dc51ef018ba066
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-01-15 02:29:56 +0500

    Write tests for reversing a string

---
+++ spec/reverse_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../solver'
+
+describe Solver do
+  solver = Solver.new
+  context "Reverse a given string" do
+    it "Reverse Hello" do
+      expect(solver.reverse("Hello")).to eq("olleH")
+    end
+    it "Reverse John" do
+      expect(solver.reverse("John")).to eq("nhoJ")
+    end
+    it "If it is a String or not" do
+      expect(solver.reverse("Hello")).to be_an_instance_of(String)
+    end
+  end
+end 
